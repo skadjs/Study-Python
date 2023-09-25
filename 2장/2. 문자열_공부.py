f18a2f78281104eff3f3b54447188576ea0a94ba
skadjs <skadjs11@gmail.com>
2023-09-26 01:39:39 +0900

    2장 공부

---
+++ 2장/2. 문자열_공부.py
@@ -0,0 +1,16 @@
+a = "My cat's name is 'ong'"
+print(a)
+
+multiline = """
+Life is too short.
+You need python.
+"""
+print(multiline)
+
+head = "Python"
+tail = " is fun"
+print(head + tail)
+
+print("=" * 50)
+print("My Program")
+print("=" * 50)
---
+++ 2장/3. 리스트_공부.py
@@ -0,0 +1,6 @@
+a = []
+b = [1, 2, 3]
+c = ['Life', 'is', 'too', 'short']
+d = [1, 2, 'Life', 'is']
+e = [1, 2, ['life', 'is']]
+
